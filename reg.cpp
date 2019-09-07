c5e1420e63f7d75b48e55e2195b7f968c030b8c0
Prashanthkoder <34128738+Prashanthkoder@users.noreply.github.com>
2019-09-07 21:07:40 +0530

    Added reg.cpp
    Does the registration process

---
+++ reg.cpp
@@ -0,0 +1,33 @@
+#include<fstream>
+#include"customer.h>
+#include<ctime>
+using namespace std;
+int reg(Customer c)
+{
+    fstream f("Customer.dat", ios::binary|ios::in|ios::out|ios::ate), logger("log.txt", ios::in|ios::out);
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    // Search if already that Customer-username is present or not
+    Customer c1;
+    int flag=0;
+    f.seekg(0, ios::beg);// move to beg to start searching
+    while(f.read((char*)&c1, sizeof(c1)))
+        if(c1.getUser()==c.getUser())
+    {
+        flag=1;
+        break;
+    }
+    if(flag==1){
+        logger<<dt<<"Tried to register "<<c1.getUser()<<" error code:001 "<<endl;
+        logger.close();
+        f.close();
+        return -1;    // username already exists
+    }
+    // If username is unique => Register
+    f.seekg(0, ios::end);// move to end to start writing
+    f.write((char*)&c, sizeof(c));
+    f.close();
+    logger<<dt<<" Successfully registered user :"<<c1.getUser()<<endl;
+    logger.close();
+    return +1;// Registration success
+}
