76f1f16cb4e5bfe64096706c24d4e65b2d15f517
Skandan Ramesh <54405114+skannorv3@users.noreply.github.com>
2019-10-08 12:53:00 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,37 @@
 #include<stdlib.h>
 int main() {
  clrscr();
- int ch,ch1,ch2,k;
+ int ch=0,ch1=0,ch2=0,k=0;
  admin dummy_admin;
- cout<<"1.enter as admin/n2.enter into application/n3.end program/n";
+ driver dummy_driver;
+ manager dummy_manager;
+ while(ch!=3)
+ {cout<<"1.enter as admin/n2.enter into application/n3.end program/n";
  cout<<"enter choice:";cin>>ch;
  if(ch==1)
- {start: k=dummy_admin.login();
-  if(k!=1){cout<<"details mismatch, retry";goto start;}
-  else cout<<"1.add new location\n2.add new driver\n3.add new manager\n";
-  cout<<"4.view returns details\n5.update a manager's details\n6.update a driver's details
+ {k=dummy_admin.login();
+  cout<<"1.add new location\n2.add new driver\n;
+  cout<<"3.view returns details\n4.update a driver's details\n;
+  if(ch==1)
+  {char locn[30];int x1,y1;
+   cout<<"enter location name:";gets(locn);
+   cout<<"enter coordinates:";cin>>x1>>y1;
+  }
+  if(ch==2)
+  {admin.driver.putdetails();}
+  if(ch==3)
+  {admin.getreturns();}
+  if(ch==4)
+  {admin.driver_update();}
+ }
+ else if(ch==2)
+ {cout<<"welcome to our taxi management application";
+  cout<<"1.book a ride\n2.exit application\n";
+  cout<<"enter choice:";cin>>ch1;
+  if(ch1==2)continue;
+  else if(ch1==1)
+  {cout<<"
+ }
  getch();
  return 0;
 }
