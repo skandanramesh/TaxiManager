8883fd0eb2d17a3148dbac9a1c0cad3a4f559deb
saran02 <54412023+saran02@users.noreply.github.com>
2019-10-08 12:26:19 +0530

    Update driver.h

--- driver.h
+++ driver.h
@@ -2,9 +2,22 @@
 #define DRIVER_INCLUDED
 
 class driver{
-  char dr_name;
-  char car_no;
+  char* dr_name;
+  char* car_no;
   double dr_no;
+  char* location;
+public: 
+  void input_data()
+  {
+    getline(dr_name);
+    getline>>car_no;
+    cin>> dr_no;
+    getline(location)
+  }
+  char* get_location()
+  {
+    return location;
+  }
 };
 
 #endif
