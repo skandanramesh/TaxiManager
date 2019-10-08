2a0b36bc4a751d0e1e3c3671fc531fcf37537769
Rahul206 <55028324+Rahul206@users.noreply.github.com>
2019-10-08 12:24:59 +0530

    Update..

--- customer.h
+++ customer.h
@@ -1,6 +1,21 @@
 #ifndef CUSTOMER_INCLUDED
 #define CUSTOMER_INCLUDED
-// Your code here...
-//hi
-//Hii
+class Customer
+{
+ char cust_user_name[30];
+ char cust_pass[30];
+ char cust_name[30];
+ char cust_phone_no[15];
+ strct Dob
+ {
+  int year;
+  int month;
+  int date;
+  void displaydob()
+  {
+   cout<<date<<"-"<<month<<"-"<<year<<'\n';
+  }
+ };
+ char cust_address[50];
+};
 #endif
