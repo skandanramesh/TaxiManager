37ace86fabc401eb8c1c3c50412623cdfd684414
Prashanthkoder <34128738+Prashanthkoder@users.noreply.github.com>
2019-10-10 22:00:20 +0530

    Create Ride.cpp

---
+++ Ride.cpp
@@ -0,0 +1,29 @@
+#include<fstream.h>
+#include<stdio.h>
+#include<process.h>
+#include<string.h>
+#include "driver.h"
+#include "locations.h"
+int taxi_booking(customer a)
+{
+    Loc pl;
+    cout<<" 1. Pickup from default address \n2.Pickup from different address \n3.Go Back"<<endl;
+    int ch;
+    cin>>ch;
+    if(ch==1)
+      pl = a.getLocation();
+    else if(ch==2)
+      getLoc(pl);
+    else return 0;
+    Driver dr = nearestDriver(pl);
+    if(dr == nullDriver){cout<<"Location currently unservicable"<<endl;return 0'}
+    cout<<"Booking confirmed. Ride Details :"<<endl;
+    Ride r(a, dr);
+    r.printDetails();
+    ofstream fout("Rides.dat", ios::binary|ios::ate);
+    fout.write((char*)&r, sizeof(Ride));
+    fout.close();
+    cout<<"Thank You for Using Our Service xD"<<endl;
+   return 1;
+}
+    
