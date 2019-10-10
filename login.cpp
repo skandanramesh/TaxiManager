9b1e48286366ae4e0b4527b6af2693d42927433c
Prashanthkoder <34128738+Prashanthkoder@users.noreply.github.com>
2019-10-10 22:27:54 +0530

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -3,12 +3,20 @@
 class Session
 {
     char* stime, etime;
-    char* user;
+   // char* user;
+    Customer c;
     int isopen; // 0 if session ended 1 if session running
 public:
-    Session(char* u)
+   /* Session(char* u)
     {
         user = u;
+        stime = gettime();
+        isOpen = 1;
+    }
+    */
+    Session(Customer c2)
+    {
+        c = c2;
         stime = gettime();
         isOpen = 1;
     }
@@ -17,14 +25,15 @@ public:
         isOpen = 0;
         etime = gettime();
         fstream logger("slog.txt", ios::in|ios::out|ios::app);
-        logger<<stime<<" to "<<etime<<" username: "<<user<<endl;
+        logger<<stime<<" to "<<etime<<" username: "<<c2.getUser();<<endl;
         logger.close();
         return 1; // session ended successfully
     }
+    Customer retCustomer(){return c;}
 
 }currentSession;
 
-Session nullSession("");
+Session nullSession(Customer(-1));
 
 Session validateUser(char* user, char* pass)
 {
@@ -36,29 +45,32 @@ Session validateUser(char* user, char* pass)
     while(f.read((char*)&c, sizeof(c))
         if(c.getUser()==user&&c.getPass()==pass)
             flag=1;
-    if(flag==0) return nullSession; // incorrect user/pass
-    return Session(user);
+    if(flag==0) {f.close();return nullSession;} // incorrect user/pass
+    f.close();
+    return Session(c);
 }
 
-int login(char* user)
+Customer login(char* user)
 {
 re:
     cout<<"1.Login 2.Go Back"<<endl;
     int ch;
     cin>>ch;
-    if(ch==2)return 0;
+    if(ch==2)return Customer(-1);
     cout<<"Please enter your password "<<endl;
     char* pass;
     gets(pass);
     currentSession = validateUser(user, pass);
     if(currentSession==nullSession)cout<<"Incorrect username or password. Please try again "<<endl;
         goto re;
-    return 1;
+    Customer c=currentSession.retCustomer();
+    return c;
 }
 int logout()
 {
     if(currentSession==nullSession||currentSession.isOpen()==0)
-        return -1;
+        return 0;
     currentSession.endSession();
     cout<<"Bye. See you later"<<endl;
+    return 1;
 }
