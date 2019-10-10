2d899891392a0d6ffd47a1173f3f151697737b9b
Prashanthkoder <34128738+Prashanthkoder@users.noreply.github.com>
2019-10-10 21:40:58 +0530

    Update driver.h

--- driver.h
+++ driver.h
@@ -3,7 +3,8 @@
 #include<iostream.h>
 #include<stdio.h>
 #include "Locations.h"
-class driver{
+#define MAX_DISTANCE 200;
+class Driver{
   char dr_name[30];
   char dr_phoneno[15];
   char car_no[10];
@@ -11,11 +12,11 @@ class driver{
   double rating;
   Loc location;
 public: 
+  Driver(){location.x=200;location.y=200;}
+  Driver(double d){rating=d;}
   void input_data()
   {
-    cout<<"Enter your name";
     gets(dr_name);
-    cout<<"Enter your car number":
     gets(car_no);
     cin>>dr_no;
     getLoc(location);
@@ -29,5 +30,23 @@ public:
     return dr_name;
   }
 };
-
+const Driver nullDriver(-1);
+Driver nearestDriver(Location pickup)
+{
+  ifstream fin("Driver.dat", ios::binary);
+  Driver temp, select;
+  int f=0;
+  if(!fin)return nullDriver;
+  fin.read((char*)&temp, sizeof(Driver));
+  select=temp;
+  while(fin.read((char*)&temp, sizeof(Driver)))
+  {
+    if(distance(temp.get_location(), pickup)<distance(select.get_location(), pickup))
+      select=temp;
+  }
+  fin.close();
+  if(distance(pickup, select.get_location())>MAX_DISTANCE)
+    return nullDriver; // No near drivers
+  return select;
+}
 #endif
