50e84246a7a9ddb3c575b7ee30b1bead87ff9730
Prashanthkoder <34128738+Prashanthkoder@users.noreply.github.com>
2019-11-04 22:51:20 +0530

    Create vehicles.h

---
+++ vehicles.h
@@ -0,0 +1,23 @@
+#ifndef VEHICLE_INCLUDED
+#define VEHICLE_INCLUDED
+class Vehicle
+{
+protected:
+   char* vno, rcno;
+   date insRenewal;   
+   double basic;
+   char* modelname;
+   char* manu;
+   int isAc;
+   char* vehType;
+  public:
+     virtual void printDetails ()=0; // pure virtual fn
+     virtual void getDetails ()=0;
+};
+class Car :public Vehicle
+{
+public:
+   Car(){vehType="Car";}
+   void printDetails (){}
+};
+#endif
