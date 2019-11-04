91db7f7486f9034b66069b59c32c583e19c010da
Prashanthkoder <34128738+Prashanthkoder@users.noreply.github.com>
2019-11-04 20:12:16 +0530

    Update rides.h

--- rides.h
+++ rides.h
@@ -1,20 +1,35 @@
 #ifndef RIDES_INCLUDED
 #define RIDES_INCLUDED
+#include<stdlib.h>
 #include "customtime.h"
 #include "driver.h"
 #include "customer.h"
+#define STD_RATE 10
+double peakfactor=1;
 class Ride
 {
     private:
       Customer c;
       Driver d;
       char* bookingTime;
+      Location pickup, drop;    
+      double fare;
     public:
-      Ride(Customer c2, Driver d2)
+      Ride(Customer c2, Driver d2, Location p, Location d)
       {
         c = c2;
         d = d2;
-        bookingTime - gettime();
+        pickup =p;
+        drop = d;  
+        fare=dist(p, d)*STD_RATE*peakfactor*(random(5)/10+1);
+        cout<<"Expected fare is "<<fare<<endl;
+      }
+    
+      void bookRide(){
+          bookingTime = gettime();
+          ofstream fout("Rides.dat", ios::binary|ios::ate);
+          fout.write((char*)&r, sizeof(Ride));
+          fout.close();
       }
       void printDetails()
       {
