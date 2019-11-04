fb6437b39d26009ba4ae935f5ff39d65599808c0
Prashanthkoder <34128738+Prashanthkoder@users.noreply.github.com>
2019-11-04 20:14:57 +0530

    Update Ride.cpp

--- Ride.cpp
+++ Ride.cpp
@@ -8,27 +8,33 @@
 #include "rides.h"
 int taxi_booking(Customer a)
 {
-    Loc pl;
-    cout<<" 1. Pickup from default address \n2.Pickup from different address \n3.Go Back"<<endl;
-    int ch;
-    cin>>ch;
-    if(ch==1)
-      pl = a.getLocation();
-    else if(ch==2)
-      getLoc(pl);
-    else return 0;
-    Driver dr = nearestDriver(pl);
-    if(dr.isNull()){cout<<"Location currently unservicable"<<endl;return 0;}
-    cout<<"Booking confirmed. Ride Details :"<<endl;
-    Ride r(a, dr);
-    r.printDetails();
-    ofstream fout("Rides.dat", ios::binary|ios::ate);
-    fout.write((char*)&r, sizeof(Ride));
-    fout.close();
-    cout<<"Thank You for Using Our Service xD"<<endl;
-    dr.incRide();
-    a.incRide ();
-    return 1;
+   cout<<" 1. Pickup from default address \n2.Pickup from different address \n3.Go Back"<<endl;
+   Loc pickup;
+   getLoc(pickup);
+   int ch;
+   cin>>ch;
+   if(ch==1)pickup = a.getLocation();
+   else if(ch==2)pickup.getLoc();
+   else return 0;
+   Driver dr = nearestDriver(pickup);
+   if(dr.isNull()){cout<<"Location currently unservicable"<<endl;return 0;}
+   cout<<"Enter Drop location"<<endl;
+   Loc drop;
+   drop.getLoc();
+   if(dist(pickup, drop)>10*MAX_DISTANCE)
+   {cout<<"Ride distance too big. We aren't servicing very long drives currently. Sorry"<<endl;return 0;} 
+   Ride r(a, dr, pickup, drop);
+   cout<<"Confirm Booking?(0-no. any other number-yes)"<<endl;
+   cin>>ch;
+   if(ch==0)return 0;
+   cout<<"Booking confirmed. Ride Details :"<<endl;
+   r.bookRide();
+   r.printDetails();
+   cout<<"Thank You for Using Our Service :)"<<endl;
+    
+   updateDriver(dr.incRide());
+   updateCustomer(a.incRide ());
+   return 1;
 }
 
 void rate_ride (Customer c)
